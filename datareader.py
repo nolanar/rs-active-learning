b156f181df563536a8b068ecbcf0d405ea4722dc
Aran Nolan <nolanar@tcd.ie>
2018-04-11 16:31:09 +0100

    Add function to get distribution of ratings for each item by group

--- datareader.py
+++ datareader.py
@@ -17,10 +17,13 @@ class DataReader:
 	data_dir = "data/"
 	figure_dir = "figures/"
 
+	# ratings should take integer values 1 to rating_value_count
 	ratings_file = data_dir + 'netflix_ratings.npz'
+	rating_value_count = 5 
 
 	cache_dir = data_dir + "_cache/"
 	nym_stats_cache_file = cache_dir + blc_data + '_nym_stats_v2.npy'
+	group_rating_dists_cache_file =  cache_dir + blc_data + '_group_rating_dists.npy'
 
 	nyms_file = data_dir + blc_data + '/P'
 	V_file = data_dir + blc_data + '/V'
@@ -111,3 +114,34 @@ class DataReader:
 	def get_lam():
 		""" number of ratings for each item by each group """
 		return DataReader.read_numpy_file(DataReader.lam_file)
+
+	@lru_cache(maxsize=1)
+	def get_group_rating_distributions():
+		cachefile = DataReader.group_rating_dists_cache_file
+		if os.path.isfile(cachefile):
+			with msg(f'Reading distribution of ratings for each item per group from "{cachefile}"'):
+				return np.load(cachefile)
+
+		with msg('Getting distribution of ratings for each item and group'):
+			R = DataReader.get_ratings()
+			P = DataReader.get_nyms()
+			group_count = DataReader.nym_count()
+			item_count = R.shape[1]
+			rating_count = DataReader.rating_value_count
+
+			dists = np.zeros((group_count, item_count, rating_count), dtype=np.float32)
+			for group_n, group in enumerate(P):
+				with msg(f'Calculating group {group_n} distributions'):
+					R_g = R[group].tocoo()
+					for item, rating in zip(R_g.col, R_g.data):
+						dists[group_n, item, int(rating - 1)] += 1
+			
+			with msg('Normalising distributions'):
+				dists /= dists.sum(axis=2, keepdims=True)
+				# nan's imply 0 ratings by group on item, so give equal distribution 
+				dists[np.isnan(dists)] = 1.0 / rating_count
+
+			with msg(f'Saving distribution of ratings to "{cachefile}"'):
+				np.save(cachefile, dists)
+
+			return dists
