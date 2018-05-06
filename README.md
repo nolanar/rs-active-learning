bc7424250b4de72f7014556b9a6aceadffcb5844
Aran Nolan <nolanar@tcd.ie>
2018-05-06 12:16:27 +0100

    Make datareader not a class and rename

--- README.md
+++ README.md
@@ -1,47 +1,16 @@
-# Active Learning for Recommender Systems
+# Accelerating Learning for Recommender Systems
 
-Active Learning for Recommender Systems via Group Learning
+Accelerating Learning for Recommender Systems via Group Learning
 
 ## Requirements
 * Python 3.6+
 
 ## Setup 
 
-1. Download the Netflix Prize data zip file from <https://www.kaggle.com/netflix-inc/netflix-prize-dat>, putting the zip file into the `data/` directory
-2. Run `./setup.sh` from the project root directory
+1. Download the Netflix Prize data zip file from <https://www.kaggle.com/netflix-inc/netflix-prize-dat>, then put the zip file into the `data/` directory.
+2. Run `./setup.sh` from the project root directory.
 
 ## Scripts
 
-### `plot_nym_stat.py`
-Plot mean or variance of each group by item number. The size of each bubble corresponds to the square root of the number of ratings for that distribution. Only bubbles with at least the threshold number of ratings are plotted.
-```
-optional arguments:
-  -h, --help         show this help message and exit
-  -o                 1 to plot mean, 2 to plot variance, (default 2)
-  -b                 index of the item to begin plotting from
-  -n                 number of items to plot
-  -t                 only plot distributions with at least threshold number of
-                     ratings (defualt 50)
-  -i                 plot inverse of chosen stat instead
-  --savefig          save the figure to file rather than displaying the figure
-  --outfile          file to save the figure to (default
-                     "figures/nym_stat.png")
- ```
-
-### `plot_item_dist.py`
-Plot of the group distributions for an item.
-```
-positional arguments:
-  item        number of item to plot
-
-optional arguments:
-  -h, --help  show this help message and exit
-  -g G        group number to plot (all by default)
-  -a          plot all ratings together ignoring groups
-  --heat      plot as 2D heatmap (defualt histogram)
-```
-
-### `datareader.py`
-Contains a utility class for getting rating and nym data. 
-
-Change the uncommented `blc_data` at the top of the config section to select which BLC generated data (nyms, V, Utilde etc.) is used.
+### `data.py`
+Utilities for getting rating and group data.
---
+++ data.py
@@ -0,0 +1,135 @@
+import numpy as np
+import scipy.sparse as sp
+import os.path
+import pathlib
+from functools import lru_cache # cache large I/O result
+
+from myutils import msg
+
+##
+# Utilities for getting rating and group data.
+##
+
+######## CONFIG #########
+# netflix data
+blc_data = '2018-04-03T18:40:29' # features: 20, groups: 8
+
+data_dir = "data/"
+figure_dir = "figures/"
+
+# ratings should take integer values 1 to rating_value_count
+ratings_file = data_dir + 'netflix_ratings.npz'
+rating_value_count = 5 
+
+cache_dir = data_dir + "_cache/"
+group_ratings_cache_file =  cache_dir + blc_data + '_group_ratings.npy'
+
+groups_file = data_dir + blc_data + '/P'
+V_file = data_dir + blc_data + '/V'
+Utilde_file = data_dir + blc_data + '/Utilde'
+lam_file = data_dir + blc_data + '/lam'
+Rvar_file = data_dir + blc_data + '/Rvar'
+P_file = data_dir + blc_data + '/P'
+#########################
+
+
+def read_numpy_file(filename, dtype=np.float32):
+	""" Read from numpy file if it exists, otherwise from raw text file """
+	with msg(f'Reading "{filename}"'):
+		if os.path.isfile(filename + ".npy"): 
+			return np.load(filename + ".npy")
+		else: 
+			return np.loadtxt(open(filename + ".txt", "r"), dtype=dtype)
+
+@lru_cache(maxsize=1)
+def get_R():
+	""" Returns the ratings matrix in compressed sparse column (csc) format.
+	Stores csc matrix to ratings_cache_file for faster loading in future.
+	Cached result to allow single load on multiple calls. 
+	"""
+	filename = DataReader.ratings_file
+	if os.path.isfile(filename):
+		with msg(f'Loading rating matrix from "{filename}"'):
+			return sp.load_npz(filename)
+	else:
+		raise RuntimeError(f'"{filename}" does not exist. Use "netflix_data.py" to generate it.')
+
+@lru_cache(maxsize=1)
+def get_groups():
+	""" Returns the groups as a list of numpy arrays.
+	Cached result to allow single load on multiple calls.
+	"""
+	P = DataReader.get_P()
+	with msg(f'Converting to list of user indexes by group'):
+		indexes = np.arange(P.shape[1])
+		return [indexes[group] for group in P.astype(bool)]
+
+@lru_cache(maxsize=1)
+def group_sizes():
+	return DataReader.get_P().sum(axis=1)
+
+def number_of_users():
+	return DataReader.get_P().shape[1]
+
+def number_of_groups():
+	return DataReader.get_P().shape[0]
+
+def group_distribution():
+	return DataReader.group_sizes() / DataReader.number_of_users()
+
+@lru_cache(maxsize=1)
+def get_Rtilde():
+	V = DataReader.read_numpy_file(DataReader.V_file)
+	Utilde = DataReader.read_numpy_file(DataReader.Utilde_file)
+	return np.dot(Utilde.T, V)
+
+@lru_cache(maxsize=1)
+def get_Rvar():
+	return DataReader.read_numpy_file(DataReader.Rvar_file)
+
+@lru_cache(maxsize=1)
+def get_lam():
+	""" number of ratings for each item by each group """
+	return DataReader.read_numpy_file(DataReader.lam_file)
+
+@lru_cache(maxsize=1)
+def get_P():
+	filename = DataReader.P_file
+	if os.path.isfile(filename + '.npz'):
+		with msg(f'Reading "{filename}.npz"'):
+			return sp.load_npz(filename + '.npz').toarray()
+	elif os.path.isfile(filename + '.npy'):
+		with msg(f'Reading "{filename}.npy"'):
+			return sp.load(filename + '.npy')
+
+@lru_cache(maxsize=1)
+def get_group_ratings():
+	"""
+	Returns a [p, m, r] array containing the ratings given by each group to each item 
+	p = number of groups
+	m = number of items
+	r = number of rating values
+	"""
+	cachefile = DataReader.group_ratings_cache_file
+	if os.path.isfile(cachefile):
+		with msg(f'Reading group ratings from "{cachefile}"'):
+			return np.load(cachefile)
+
+	with msg('Getting group ratings'):
+		R = DataReader.get_ratings()
+		P = DataReader.get_groups()
+		number_of_groups = DataReader.number_of_groups()
+		item_count = R.shape[1]
+		rating_count = DataReader.rating_value_count
+
+		ratings = np.zeros((number_of_groups, item_count, rating_count), dtype=np.float32)
+		with msg(f'Calculating rating counts'):
+			for group_n, group in enumerate(P):
+				for rating_index in range(rating_count):
+					rating = rating_index + 1
+					ratings[group_n, :, rating_index] = (R[group] == rating).sum(axis=0)
+
+		with msg(f'Saving group ratings to "{cachefile}"'):
+			np.save(cachefile, ratings)
+
+		return ratings
